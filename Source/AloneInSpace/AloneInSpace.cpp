1f29262b9eb4bc588a3c8b80262582a466dd5485
Sergey Firsov <fearthunder@yandex.ru>
2021-04-10 14:44:40 +0300

    Initial commit

---
+++ Source/AloneInSpace/AloneInSpace.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AloneInSpace.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AloneInSpace, "AloneInSpace" );
---
+++ Source/AloneInSpace/AloneInSpaceGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AloneInSpaceGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ALONEINSPACE_API AAloneInSpaceGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
