3887153b4aca90ed6d2ad52a0209e91a9e53f34d
Sergey Firsov <fearthunder@yandex.ru>
2021-04-10 15:38:00 +0300

    Add OxygenComponent

---
+++ Source/AloneInSpace/Components/OxygenComponentC.cpp
@@ -0,0 +1,71 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "OxygenComponentC.h"
+
+// Sets default values for this component's properties
+UOxygenComponentC::UOxygenComponentC()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+void UOxygenComponentC::AddOxygen(float OxygenAmount)
+{
+	Oxygen += OxygenAmount;
+
+	if (Oxygen > OxygenMax)
+	{
+		Oxygen = OxygenMax;
+	}
+}
+
+void UOxygenComponentC::RemoveOxygen(float OxygenDamage)
+{
+	Oxygen -= OxygenDamage;
+
+	if (Oxygen < 0.0f)
+	{
+		Oxygen = 0.0f;
+	}
+}
+
+float UOxygenComponentC::GetOxygen()
+{
+	return Oxygen;
+}
+
+void UOxygenComponentC::AddOxygenSpending(float OxySpend)
+{
+	OxygenSpending += OxySpend;
+}
+
+void UOxygenComponentC::RemoveOxygenSpending(float OxySpend)
+{
+	OxygenSpending -= OxySpend;
+
+	if (OxygenSpending < 0.0f)
+	{
+		OxygenSpending = 0.0f;
+	}
+}
+
+
+// Called when the game starts
+void UOxygenComponentC::BeginPlay()
+{
+	Super::BeginPlay();
+
+	StartOxygenSpending();
+}
+
+void UOxygenComponentC::OxygenSpend()
+{
+	RemoveOxygen(OxygenSpending * OxygenSpendingRate);
+}
+
+void UOxygenComponentC::StartOxygenSpending()
+{
+	GetWorld()->GetTimerManager().SetTimer(OxygenSpendTimer, this, &UOxygenComponentC::OxygenSpend, OxygenSpendingRate);
+}
---
+++ Source/AloneInSpace/Components/OxygenComponentC.h
@@ -0,0 +1,55 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "OxygenComponentC.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(IsBlueprintBase = "true") )
+class ALONEINSPACE_API UOxygenComponentC : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UOxygenComponentC();
+
+	UFUNCTION(BlueprintCallable, Category = "Oxygen")
+	void AddOxygen(float OxygenAmount);
+
+	UFUNCTION(BlueprintCallable, Category = "Oxygen")
+	void RemoveOxygen(float OxygenDamage);
+
+	UFUNCTION(BlueprintPure, Category = "Oxygen")
+	float GetOxygen();
+
+	UFUNCTION(BlueprintCallable, Category = "Oxygen")
+	void AddOxygenSpending(float OxySpend);
+
+	UFUNCTION(BlueprintCallable, Category = "Oxygen")
+	void RemoveOxygenSpending(float OxySpend);
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Oxygen")
+	float Oxygen;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Oxygen")
+	float OxygenMax;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Oxygen")
+	float OxygenSpending;
+	
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Oxygen")
+	float OxygenSpendingRate;
+
+	void OxygenSpend();
+
+	void StartOxygenSpending();
+
+	FTimerHandle OxygenSpendTimer;
+};
